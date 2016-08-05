1d282b8fc0166322f06fb0f0a8b4a7d5b55b1ec7
TylerMoeller <tylermoeller1@gmail.com>
2016-08-05 11:38:39 -0700

    Add survey and update index with absolute urls
    Updated index.html with absolute URLs. Updated islandjs.css indenting
    and switched to LF line endings. Added survey.html, survey.js, and
    survey.css. These will need be verified against the server by @awaterma
    - more code changes may be required later as a result.

---
+++ js/survey.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  $('form').submit(function (event) {
+    event.preventDefault();
+
+    var weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+        bestDays = [],
+        venueRating = [],
+        volunteerChoices = [],
+        langs = [];
+
+    // multi-choice answers go to an array
+    $('input[id^=venue]:checked').filter(function () {
+      venueRating.push($(this).val());
+    });
+
+    weekdays.forEach(function (day) {
+      bestDays.push($('input[name=' + day + ']:checked').val());
+    });
+
+    $('input[id^=volunteer]:checked').filter(function () {
+      volunteerChoices.push($(this).val());
+    });
+
+    $('input[id^=langs]:checked').filter(function () {
+      langs.push($(this).val());
+    });
+
+    var results = {
+      Survey: 'NodeSchool',
+      LastEventEnjoyed: $('input[name=enjoy-last-event]:checked').val(),
+      LastEventFound: $('input[id=find-last-event]').val(),
+      LastEventVenue: venueRating,
+      LastEventFeedback: $('input[id=venuefeedback]').val(),
+      MakeNextEventBetter: $('input[id=make-event-better]').val(),
+      UpcomingEventInterest: $('input[name=enjoy-next-event]:checked').val(),
+      BestDayAndTimes: bestDays,
+      FutureTopicSuggestions: $('input[id=future-topics]').val(),
+      PositiveChange: $('input[name=positive-change]:checked').val(),
+      ShareYourProjects: $('input[name=personal-project-share]:checked').val(),
+      ContributeToProjects: $('input[name=project-contribute]:checked').val(),
+      Volunteer: volunteerChoices,
+      ServerSideDev: $('input[name=server-side-dev]:checked').val(),
+      YearsDeveloping: $('input[name=dev-years]:checked').val(),
+      CodeLanguages: langs,
+      AdditionalLangs: $('#otherlangs').val(),
+      ContactInfo: $('#contact').val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      crossDomain: true,
+      data: JSON.stringify(results),
+      dataType: 'json'
+    })
+    .done(function (response) {
+      if (localStorage) {
+        localStorage.setItem('survey', 'completed');
+      } else {
+        $.cookie('survey', 'completed');
+      }
+
+      window.history.back().reload();
+    })
+    .fail(function (xhr, status) {
+      $('#error-modal').openModal();
+    });
+  });
+
+  $('#close-button').click(function () {
+    window.history.back();
+  });
+});
